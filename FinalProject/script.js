46679ec12c8e533a9665300904d01852fa317903
Kyle <a118361@aup.edu>
2025-01-24 13:11:17 +0100

    Margin added

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,6 +1,7 @@
 function toggleImage(imageId) {
     var img = document.getElementById(imageId);
     if (img.style.display == "none") {
+        // Show the image in a new block element
         img.style.display = "block";
     } else {
         img.style.display = "none";
