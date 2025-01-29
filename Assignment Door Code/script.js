8862d876fe2947d097a2d2d56f64e3053e1e14f8
Kyle <a118361@aup.edu>
2025-01-29 14:32:50 +0100

    Started door code assignment

---
+++ Assignment Door Code/script.js
@@ -0,0 +1,7 @@
+function buttonClick(button) {
+    if (button.textContent === "1" || button.textContent === "2"
+        || button.textContent === "3") {
+        button.style.backgroundColor = "green";
+    }
+
+}
