660aa099112adc320e8ce073d2deb33d3d218bfb
Kyle <a118361@aup.edu>
2025-02-14 11:24:39 +0100

    Revert

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,129 +1,127 @@
-// let scrollInterval;
-// let speedTimeout;
-// let currentScrollPosition = 0;
-// let currentScrollSpeed = 0;
-// let scrolling = false;
-// let scrollSpeed;
-// let imageInView = false;
+let scrollInterval;
+let speedTimeout;
+let currentScrollPosition = 0;
+let currentScrollSpeed = 0;
+let scrolling = false;
+let scrollSpeed;
+let imageInView = false;
 
-// function resetScrolling(iframeId, initialScrollSpeed) {
-//     pauseScrolling(); // Stop any ongoing scrolling
-//     const iframe = document.getElementById(iframeId);
-//     const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-//     iframeDocument.documentElement.scrollTop = 0; // Scroll to the top
-//     iframeDocument.body.scrollTop = 0; // Scroll to the top
-//     currentScrollPosition = 0; // Reset the current scroll position
-//     currentScrollSpeed = initialScrollSpeed; // Reset the current scroll speed
-//     iframe.contentWindow.scrollTo(0, 0);
-//     scrolling = false;
-//     imageInView = false;
-// }
+function toggleIframeAndButtons(iframeId, buttonContainerId, button) {
+    const iframe = document.getElementById(iframeId);
+    const buttonContainer = document.getElementById(buttonContainerId);
+    const containerMusicTrack = button.closest('.containerMusicTrack'); // Get the closest guitarTab container
+    const siblingButtons = containerMusicTrack.querySelectorAll('.buttonShow'); // Get all buttons within the same container
 
-// function startScrolling(iframeId, speed, tempoChangePageID, tempoChangeSpeed) {
-//     pauseScrolling(); // Ensure any existing scrolling is stopped before starting a new one
+    // Define the IDs of the other iframes and their button containers
+    const otherIframeId = iframeId === 'iframeParanoidLead' ? 'iframeParanoidRhythm' : 'iframeParanoidLead';
+    const otherButtonContainerId = buttonContainerId === 'buttonContainerLead' ? 'buttonContainerRhythm' : 'buttonContainerLead';
 
-//     const iframe = document.getElementById(iframeId);
-//     const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-//     let scrollSpeed = currentScrollSpeed || speed;
-//     let accumulatedScroll = 0;
+    const otherIframe = document.getElementById(otherIframeId);
+    const otherButtonContainer = document.getElementById(otherButtonContainerId);
 
-//     // Function to increase scroll speed when image is in view
-//     function increaseScrollSpeed(entries) {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 scrollSpeed = tempoChangeSpeed;
-//                 currentScrollSpeed = scrollSpeed; // Maintain the current scroll speed
-//             }
-//         });
-//     }
+    if (iframe.style.display === "none" || iframe.style.display === "") {
+        iframe.style.display = "block";
+        buttonContainer.style.display = "flex";
+        button.textContent = "Hide";
+        buttonContainer.scrollIntoView({ behavior: 'smooth' }); // Scroll to the iframe smoothly
 
-//     // Create an IntersectionObserver
-//     const observer = new IntersectionObserver(increaseScrollSpeed, {
-//         root: iframeDocument,
-//         threshold: 0.1
-//     });
+        // Hide sibling buttons
+        siblingButtons.forEach(siblingButton => {
+            if (siblingButton !== button) {
+                siblingButton.style.display = "none";
+            }
+        });
 
-//     // Observe the target image
-//     const targetImage = iframeDocument.getElementById(tempoChangePageID);
-//     if (targetImage) {
-//         observer.observe(targetImage);
-//     }
+        // Hide the other iframe and its button container
+        otherIframe.style.display = "none";
+        otherButtonContainer.style.display = "none";
+    } else {
+        resetScrolling(iframeId, 0); // Reset scrolling when the iframe is hidden
+        iframe.style.display = "none";
+        buttonContainer.style.display = "none";
+        button.textContent = button.getAttribute('data-original-text'); // Restore original text
+        containerMusicTrack.scrollIntoView({ behavior: 'smooth' }); // Scroll to the top of the guitarTab container smoothly
 
-//     // Function to scroll the iframe
-//     function scrollIframe() {
-//         accumulatedScroll += scrollSpeed;
-//         const scrollAmount = Math.floor(accumulatedScroll);
-//         accumulatedScroll -= scrollAmount;
+        // Show sibling buttons
+        siblingButtons.forEach(siblingButton => {
+            siblingButton.style.display = "inline-block";
+        });
+    }
+}
 
-//         iframe.contentWindow.scrollBy(0, scrollAmount);
-//         if (scrolling) {
-//             requestAnimationFrame(scrollIframe);
-//         }
-//     }
-
-//     // Start scrolling
-//     scrolling = true;
-//     scrollIframe();
-// }
-
-// function pauseScrolling() {
-//     scrolling = false;
-// }
-
-// function resetScrolling(iframeId) {
-//     const iframe = document.getElementById(iframeId);
-//     const contentWindow = iframe.contentWindow;
-
-//     pauseScrolling();
-//     contentWindow.scrollTo(0, 0);
-//     currentScrollSpeed = null;
-// }
-
-let scrollAnimationFrame;
-
-function startScrolling(iframeId, duration) {
+function resetScrolling(iframeId, initialScrollSpeed) {
+    pauseScrolling(); // Stop any ongoing scrolling
     const iframe = document.getElementById(iframeId);
     const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-    const element = iframeDocument.documentElement; // Use documentElement for scrolling
+    iframeDocument.documentElement.scrollTop = 0; // Scroll to the top
+    iframeDocument.body.scrollTop = 0; // Scroll to the top
+    currentScrollPosition = 0; // Reset the current scroll position
+    currentScrollSpeed = initialScrollSpeed; // Reset the current scroll speed
+    iframe.contentWindow.scrollTo(0, 0);
+    scrolling = false;
+    imageInView = false;
+}
 
-    const totalHeight = element.scrollHeight - element.clientHeight;
-    const startTime = performance.now();
+function startScrolling(iframeId, speed, tempoChangePageID, tempoChangeSpeed) {
+    if (scrolling) {
+        return;
+    } 
+    
+    pauseScrolling(); // Ensure any existing scrolling is stopped before starting a new one
 
-    const step = (currentTime) => {
-        const elapsedTime = currentTime - startTime;
-        const progress = Math.min(elapsedTime / duration, 1);
-        element.scrollTop = progress * totalHeight;
+    const iframe = document.getElementById(iframeId);
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    let scrollSpeed = currentScrollSpeed || speed;
+    let accumulatedScroll = 0;
 
-        if (progress < 1) {
-            scrollAnimationFrame = window.requestAnimationFrame(step);
+    // Function to increase scroll speed when image is in view
+    function increaseScrollSpeed(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                scrollSpeed = tempoChangeSpeed;
+                currentScrollSpeed = scrollSpeed; // Maintain the current scroll speed
+            }
+        });
+    }
+
+    // Create an IntersectionObserver
+    const observer = new IntersectionObserver(increaseScrollSpeed, {
+        root: iframeDocument,
+        threshold: 0.1
+    });
+
+    // Observe the target image
+    const targetImage = iframeDocument.getElementById(tempoChangePageID);
+    if (targetImage) {
+        observer.observe(targetImage);
+    }
+
+    // Function to scroll the iframe
+    function scrollIframe() {
+        accumulatedScroll += scrollSpeed;
+        const scrollAmount = Math.floor(accumulatedScroll);
+        accumulatedScroll -= scrollAmount;
+
+        iframe.contentWindow.scrollBy(0, scrollAmount);
+        if (scrolling) {
+            requestAnimationFrame(scrollIframe);
         }
-    };
+    }
 
-    scrollAnimationFrame = window.requestAnimationFrame(step);
+    // Start scrolling
+    scrolling = true;
+    scrollIframe();
 }
 
 function pauseScrolling() {
-    if (scrollAnimationFrame) {
-        window.cancelAnimationFrame(scrollAnimationFrame);
-    }
+    scrolling = false;
 }
 
 function resetScrolling(iframeId) {
-    pauseScrolling();
     const iframe = document.getElementById(iframeId);
-    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-    const element = iframeDocument.documentElement; // Use documentElement for scrolling
-    element.scrollTop = 0;
-}
+    const contentWindow = iframe.contentWindow;
 
-function toggleIframeAndButtons(iframeId, containerId, button) {
-    const iframe = document.getElementById(iframeId);
-    const container = document.getElementById(containerId);
-    if (container.style.display === 'none') {
-        container.style.display = 'block';
-        button.textContent = button.getAttribute('data-original-text');
-    } else {
-        container.style.display = 'none';
-        button.textContent = 'Show ' + button.getAttribute('data-original-text');
-    }
+    pauseScrolling();
+    contentWindow.scrollTo(0, 0);
+    currentScrollSpeed = null;
 }
