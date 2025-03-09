4ac820172493310c17103f76ba480b743ef6a161
ktkt9898 <kyletruschel@u.boisestate.edu>
2025-03-09 15:50:16 +0100

    Notes added for clarity

--- Final-Project/script.js
+++ Final-Project/script.js
@@ -1,7 +1,9 @@
 let scrollInterval;
 let speedTimeout;
 let currentScrollPosition = 0;
-let currentScrollSpeed = 0;
+
+// Stores the speed adjusted from tempo changes
+let currentScrollSpeed;
 let scrolling = false;
 let scrollSpeed;
 let imageInView = false;
@@ -31,59 +33,107 @@ document.getElementById('close-btn').addEventListener('click', function() {
     warningClosed = true; // Set the flag to true when the warning is closed
 });
 
+/* 
+Takes in an ID for an iFrame, ID for the button container style, and the "this" parameter for
+the actual Show/Hide button to toggle the menu 
+
+Each iFrame has an ID that maps to the individual HTML files under tabs. This displays
+the sheet music within a smaller window to be scrolled manually or automatically
+*/
 function toggleIframeAndButtons(iframeId, buttonContainerId, button) {
     const iframe = document.getElementById(iframeId);
     const buttonContainer = document.getElementById(buttonContainerId);
-    const containerMusicTrack = button.closest('.containerMusicTrack'); // Get the closest guitarTab container
-    const siblingButtons = containerMusicTrack.querySelectorAll('.buttonShow'); // Get all buttons within the same container
 
-    // Define the IDs of the other iframes and their button containers
+    /* 
+    Get the containerMusicTrack and sibling buttons, this is retrieved now to later be used for the smooth
+    scrolling effect when the iframe is shown
+    */
+    const containerMusicTrack = button.closest('.containerMusicTrack');
+
+    /* 
+    Retrieve all the buttons within the previous containerMusicTrack
+    Naming convention is buttonShow because this is the CSS style for all buttons, onced viewable/shown
+    */
+    const siblingButtons = containerMusicTrack.querySelectorAll('.buttonShow');
+
+    /*
+    Create a new variable for either Rythm or Lead sheet music, and hide the other if one is chosen.
+    Ternary operation is used for the other sheet music option, Lead or Rhythm. This is stored later to be hidden
+    from view.
+    */
     const otherIframeId = iframeId === 'iframeLead' ? 'iframeRhythm' : 'iframeLead';
     const otherButtonContainerId = buttonContainerId === 'buttonContainerLead' ? 'buttonContainerRhythm' : 'buttonContainerLead';
-
     const otherIframe = document.getElementById(otherIframeId);
     const otherButtonContainer = document.getElementById(otherButtonContainerId);
 
+    /*
+    If the iframe is hidden or not displayed, show the iframe and button container
+    Set up the buttons to be displayed in block format, with a flex container for the buttons
+    Set the button text to "Hide", from Show, and scroll to the iframe smoothly
+    */
     if (iframe.style.display === "none" || iframe.style.display === "") {
         iframe.style.display = "block";
         buttonContainer.style.display = "flex";
         button.textContent = "Hide";
         buttonContainer.scrollIntoView({ behavior: 'smooth' }); // Scroll to the iframe smoothly
 
-        // Hide sibling buttons
+        /* Hide sibling buttons IF not contained from the original button click, from the other Lead or Rhythm 
+        sheet music option */
         siblingButtons.forEach(siblingButton => {
             if (siblingButton !== button) {
                 siblingButton.style.display = "none";
             }
         });
 
-        // Hide the other iframe and its button container
+        // Hide the other iFrame, Lead or Rhythm depending on what was chosen, and its button container
         otherIframe.style.display = "none";
         otherButtonContainer.style.display = "none";
-    } else {
-        resetScrolling(iframeId, 0); // Reset scrolling when the iframe is hidden
+    }
+
+    else {
+        resetScrolling(iframeId, 0); // Reset scrolling when the iFrame is hidden, meaning "Hide" was clicked
         iframe.style.display = "none";
         buttonContainer.style.display = "none";
         button.textContent = button.getAttribute('data-original-text'); // Restore original text
         containerMusicTrack.scrollIntoView({ behavior: 'smooth' }); // Scroll to the top of the guitarTab container smoothly
 
-        // Show sibling buttons
+        // Display the Lead or Rhythm options again, previously was hidden when one choice was selected instead of the other
         siblingButtons.forEach(siblingButton => {
             siblingButton.style.display = "inline-block";
         });
     }
 }
 
-function startScrolling(iframeId, speed, backingTrackId, tempoChanges) {
+/*
+Function to automically scroll the iframe, takes in the iframe ID, speed, backing track ID, and tempo changes
+
+Tempo changes are used to increase the scroll speed when a certain part of the sheet music is in view.
+In reality, an observer is used to detect a transparent square that is placed over the sheet music image.
+*/
+function startScrolling(iframeId, originalSpeed, backingTrackId, tempoChanges) {
+    /* Safety check to prevent multiple scrolling instances, if true, end the function 
+    (return block execute lways ends a function) */
     if (scrolling) {
         return;
     } 
 
-    pauseScrolling(); // Ensure any existing scrolling is stopped before starting a new one
+    /* Ensure any existing scrolling is paused before starting a new one
+    Also pauses any audio */
+    pauseScrolling(); 
 
+    // Retrive the iFrame ID, this is where the sheet music is displayed
     const iframe = document.getElementById(iframeId);
+    /* 
+    Also retrive the entire DOM of the HTML file, recall the iFrame is a separate HTML file and is located
+    under tabs
+    */
     const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-    let scrollSpeed = currentScrollSpeed || speed;
+
+    // scrollSpeed will attempt to be assigned to the currentScrollSpeed, as modified from the tempo changes,
+    // But in the instance of the first function call, the original passed in parameter speed is used.
+    let scrollSpeed = currentScrollSpeed || originalSpeed;
+
+    // Accumulated scroll is used to keep track of the fractional part of the scroll speed
     let accumulatedScroll = 0;
 
     // Play the audio if it exists
@@ -92,26 +142,17 @@ function startScrolling(iframeId, speed, backingTrackId, tempoChanges) {
         audio.play();
     }
 
-    // Function to increase scroll speed when image is in view
-    function increaseScrollSpeed(entries) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const tempoChange = tempoChanges.find(change => change.tempoChangePageID === entry.target.id);
-                if (tempoChange) {
-                    scrollSpeed = tempoChange.tempoChangeSpeed;
-                    currentScrollSpeed = scrollSpeed; // Maintain the current scroll speed
-                }
-            }
-        });
-    }
-
-    // Create an IntersectionObserver
+        /*
+    The observer is used to detect when the transparent square is in view, which is placed over the sheet music image.
+    When the square is in view, the scroll speed is increased to the tempo change speed.
+    */
     const observer = new IntersectionObserver(increaseScrollSpeed, {
         root: iframeDocument,
+
+        // Threshold of 1.0 means the entire target
         threshold: 1.0
     });
 
-    // Observe the target images
     tempoChanges.forEach(change => {
         const targetImage = iframeDocument.getElementById(change.tempoChangePageID);
         if (targetImage) {
@@ -119,8 +160,36 @@ function startScrolling(iframeId, speed, backingTrackId, tempoChanges) {
         }
     });
 
+    /* 
+    Function to increase scroll speed when image is in view
+    entires parameter is provided by the observer API, and is an array of IntersectionObserverEntry objects
+    */
+    function increaseScrollSpeed(entries) {
+        entries.forEach(entry => {
+            // If the transparent square is in view, the conditional block is executed; it is true
+            if (entry.isIntersecting) {
+                /*
+                Tempo change values are stored as an array, with a corresponding ID and speed value
+                Entry.target.id is the ID of the image in the sheet music that the observer API is watching
+                Once intersecting, assign the tempoChangeID array value to the entry.target.id
+                */
+                const tempoChange = tempoChanges.find(change => change.tempoChangePageID === entry.target.id);
+                // If the tempo change is found, true, set the scroll speed to the tempo change speed
+                if (tempoChange) {
+                    scrollSpeed = tempoChange.tempoChangeSpeed;
+                    // currentScrollSpeed is now set to the tempo change speed
+                    currentScrollSpeed = scrollSpeed;
+                }
+            }
+        });
+    }
+
     // Function to scroll the iframe
     function scrollIframe() {
+        /*
+        accumulatedScroll is initially zero, but will be updated in accordance with the tempo changes
+        modified from the increaseScrollSpeed function
+        */
         accumulatedScroll += scrollSpeed;
         const scrollAmount = Math.floor(accumulatedScroll);
         accumulatedScroll -= scrollAmount;
