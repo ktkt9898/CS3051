8422c25b0847fff06989923330277fa966a394d9
ktkt9898 <kyletruschel@gmail.com>
2025-03-17 14:14:18 +0100

    17-Mar-25, comments updated

--- Final-Project/script.js
+++ Final-Project/script.js
@@ -5,6 +5,9 @@ let currentSpeed;
 let elapsedTime = 0;
 let scrolling = false;
 
+// Flag to stop the operation of the scrolling, if Hide is clicked
+let stopOperation = false; 
+
 // Stores the speed adjusted from tempo changes
 let warningClosed = false;
 
@@ -16,7 +19,9 @@ function checkScreenDimensions() {
         }
     } else {
         warning.style.display = 'none';
-        warningClosed = false; // Reset the flag when dimensions are greater than 800
+
+        // Reset the flag when dimensions are greater than 800
+        warningClosed = false; 
     }
 }
 
@@ -32,7 +37,7 @@ document.getElementById('close-btn').addEventListener('click', function() {
     warningClosed = true; // Set the flag to true when the warning is closed
 });
 
-/* 
+/**
 Takes in an ID for an iFrame, ID for the button container style, and the "this" parameter for
 the actual Show/Hide button to toggle the menu 
 
@@ -43,19 +48,19 @@ function toggleIframeAndButtons(iframeId, buttonContainerId, button) {
     const iframe = document.getElementById(iframeId);
     const buttonContainer = document.getElementById(buttonContainerId);
 
-    /* 
+    /**
     Get the containerMusicTrack and sibling buttons, this is retrieved now to later be used for the smooth
     scrolling effect when the iframe is shown
     */
     const containerMusicTrack = button.closest('.containerMusicTrack');
 
-    /* 
+    /**
     Retrieve all the buttons within the previous containerMusicTrack
     Naming convention is buttonShow because this is the CSS style for all buttons, onced viewable/shown
     */
     const siblingButtons = containerMusicTrack.querySelectorAll('.buttonShow');
 
-    /*
+    /**
     Create a new variable for either Rythm or Lead sheet music, and hide the other if one is chosen.
     Ternary operation is used for the other sheet music option, Lead or Rhythm. This is stored later to be hidden
     from view.
@@ -65,7 +70,7 @@ function toggleIframeAndButtons(iframeId, buttonContainerId, button) {
     const otherIframe = document.getElementById(otherIframeId);
     const otherButtonContainer = document.getElementById(otherButtonContainerId);
 
-    /*
+    /**
     If the iframe is hidden or not displayed, show the iframe and button container
     Set up the buttons to be displayed in block format, with a flex container for the buttons
     Set the button text to "Hide", from Show, and scroll to the iframe smoothly
@@ -74,10 +79,14 @@ function toggleIframeAndButtons(iframeId, buttonContainerId, button) {
         iframe.style.display = "block";
         buttonContainer.style.display = "flex";
         button.textContent = "Hide";
-        buttonContainer.scrollIntoView({ behavior: 'smooth' }); // Scroll to the iframe smoothly
 
-        /* Hide sibling buttons IF not contained from the original button click, from the other Lead or Rhythm 
-        sheet music option */
+        // Scroll to the iframe smoothly
+        buttonContainer.scrollIntoView({ behavior: 'smooth' }); 
+
+        /**
+        Hide sibling buttons IF not contained from the original button click, from the other Lead or Rhythm 
+        sheet music option 
+        */
         siblingButtons.forEach(siblingButton => {
             if (siblingButton !== button) {
                 siblingButton.style.display = "none";
@@ -90,7 +99,11 @@ function toggleIframeAndButtons(iframeId, buttonContainerId, button) {
     }
 
     else {
-        resetScrolling(iframeId, 0); // Reset scrolling when the iFrame is hidden, meaning "Hide" was clicked
+        // Set the flag to stop the operation of the scrolling, if Hide is clicked
+        stopOperation = true; 
+
+        // Reset scrolling when the iFrame is hidden, meaning "Hide" was clicked
+        resetScrolling(iframeId, 0);
         iframe.style.display = "none";
         buttonContainer.style.display = "none";
         button.textContent = button.getAttribute('data-original-text'); // Restore original text
@@ -103,13 +116,12 @@ function toggleIframeAndButtons(iframeId, buttonContainerId, button) {
     }
 }
 
-/*
+/**
 Function to automically scroll the iframe, takes in the iframe ID, speed, backing track ID, and tempo changes
 
 Tempo changes are used to increase the scroll speed when a certain part of the sheet music is in view.
 In reality, an observer is used to detect a transparent square that is placed over the sheet music image.
 */
-
 function showCountdown(callback) {
     const countdownContainer = document.createElement('div');
     countdownContainer.id = 'countdown-container';
@@ -129,6 +141,12 @@ function showCountdown(callback) {
     countdownContainer.textContent = countdown;
 
     const interval = setInterval(() => {
+        // If Hide was clicked, stop the operation and remove the countdown container
+        if (stopOperation) {
+            clearInterval(interval);
+            document.body.removeChild(countdownContainer);
+            return;
+        }
         countdown--;
         if (countdown > 0) {
             countdownContainer.textContent = countdown;
@@ -142,7 +160,7 @@ function showCountdown(callback) {
     }, 1000);
 }
 
-/*
+/**
 The startScrolling function performs the main feature of the automatic scrolling of the sheet music.
 It takes in the iframe ID, initial speed, backing track ID, and speed changes.
 Speed changes are an array of objects that contain the time and speed change.
@@ -157,89 +175,97 @@ updateElapsedTime is a recursive function that keeps track of the elapsed time w
 speedChanges is an array that containts a time and speed change, since some parts of the sheet music may be faster or slower.
 */
 function startScrolling(iframeId, initialSpeed, backingTrackId, speedChanges) {
-    const iframe = document.getElementById(iframeId);
-    const contentWindow = iframe.contentWindow;
-    const audio = document.getElementById(backingTrackId);
-
-    // Use the current speed if it exists, otherwise use the initial speed
-    currentSpeed = currentSpeed || initialSpeed;
-
-    // Start keeping track of the elapsed time, so pausing can resume at the same exact position
-    let startTime = Date.now();
-
-    // Function to perform the scrolling
-    function scrollContent() {
-        if (scrolling) {
-            currentScrollPosition += currentSpeed;
-            contentWindow.scrollTo(0, currentScrollPosition);
-            requestAnimationFrame(scrollContent); // Schedule the next scroll
+    // Reset the flag to false when starting the scrolling operation
+    stopOperation = false; 
+    showCountdown(() => {
+        if (stopOperation) {
+            return;
         }
-    }
+        const iframe = document.getElementById(iframeId);
+        const contentWindow = iframe.contentWindow;
+        const audio = document.getElementById(backingTrackId);
 
-    // Start the audio if it exists
-    if (audio) {
-        audio.play();
-    }
+        // Use the current speed if it exists, otherwise use the initial speed
+        currentSpeed = currentSpeed || initialSpeed;
 
-    // Start the initial scrolling
-    scrolling = true;
+        // Start keeping track of the elapsed time, so pausing can resume at the same exact position
+        let startTime = Date.now();
 
-    /*
-    Cap to 60Hz to maintain consistent scrolling speed across devices
-    1000 / 60 is used to approimate 16.67ms which corresponds to 60Hz refresh rate
-    So, set the scrollContent speed interval to update every 16.67ms, or a 60hz refresh rate
-    */
-    requestAnimationFrame(scrollContent);
+        // Function to perform the scrolling
+        function scrollContent() {
+            if (scrolling && !stopOperation) {
+                currentScrollPosition += currentSpeed;
+                contentWindow.scrollTo(0, currentScrollPosition);
 
-    /*
-    Speed changes are the array passed in that contains the time interval and associated
-    speed change
-    If the specified time is greater than the elapsed time, set a timeout to change the speed
-    The variable "change" is the element in the array that contains the time and speed change
-    The adjustedTime is the time minus the elapsed time, and if it is greater than 0, set a timeout
-    */
-    speedChanges.forEach(change => {
-        const adjustedTime = change.time - elapsedTime;
-        if (adjustedTime > 0) {
-            const timeout = setTimeout(() => {
-                currentSpeed = change.speed;
-            }, adjustedTime);
-            // Store the timeout to be cleared later, when pausing or reseting the scrolling
-            speedTimeouts.push(timeout);
+                // Schedule the next scroll
+                requestAnimationFrame(scrollContent); 
+            }
         }
+
+        // Start the audio if it exists
+        if (audio) {
+            audio.play();
+        }
+
+        // Start the initial scrolling
+        scrolling = true;
+
+        /**
+        Cap to 60Hz to maintain consistent scrolling speed across devices
+        1000 / 60 is used to approimate 16.67ms which corresponds to 60Hz refresh rate
+        So, set the scrollContent speed interval to update every 16.67ms, or a 60hz refresh rate
+        */
+        requestAnimationFrame(scrollContent);
+
+        /**
+        Speed changes are the array passed in that contains the time interval and associated
+        speed change
+        If the specified time is greater than the elapsed time, set a timeout to change the speed
+        The variable "change" is the element in the array that contains the time and speed change
+        The adjustedTime is the time minus the elapsed time, and if it is greater than 0, set a timeout
+        */
+        speedChanges.forEach(change => {
+            const adjustedTime = change.time - elapsedTime;
+            if (adjustedTime > 0) {
+                const timeout = setTimeout(() => {
+                    currentSpeed = change.speed;
+                }, adjustedTime);
+
+                // Store the timeout to be cleared later, when pausing or reseting the scrolling
+                speedTimeouts.push(timeout);
+            }
+        });
+
+        /** 
+        Check if scrolling is active, and ensure the elapsed time is continuously updated
+        The global variable "elapsedTime" is updated to keep track of the time when the 
+        scrolling is paused.
+        The new elapsedTime is the current time minus the start time, and is used to update the
+        adjustedTime variable.
+        updateElapsedTime is recursively called to keep track of the elapsed time while scrolling is true
+        */
+        function updateElapsedTime() {
+            if (scrolling && !stopOperation) {
+                elapsedTime += Date.now() - startTime;
+                startTime = Date.now();
+                requestAnimationFrame(updateElapsedTime);
+            }
+        }
+        updateElapsedTime();
     });
-
-    /*
-    Check if scrolling is active, and ensure the elapsed time is continuously updated
-
-    The global variable "elapsedTime" is updated to keep track of the time when the 
-    scrolling is paused.
-    The new elapsedTime is the current time minus the start time, and is used to update the
-    adjustedTime variable.
-
-    updateElapsedTime is recursively called to keep track of the elapsed time while scrolling is true
-    */
-    function updateElapsedTime() {
-        if (scrolling) {
-            elapsedTime += Date.now() - startTime;
-            startTime = Date.now();
-            requestAnimationFrame(updateElapsedTime);
-        }
-    }
-    updateElapsedTime();
 }
 
 function pauseScrolling(backingTrackId) {
     scrolling = false;
 
     // Pause the audio if it exists
-    const audio = document.getElementById(backingTrackId);
+    const audio = document.getElementById(backingTrackId); 
     if (audio) {
         audio.pause();
     }
 
     // Clear the scroll interval and speed timeouts
-    clearInterval(scrollInterval);
+    clearInterval(scrollInterval); 
     speedTimeouts.forEach(timeout => clearTimeout(timeout));
     speedTimeouts = [];
 }
@@ -248,6 +274,11 @@ function resetScrolling(iframeId, backingTrackId) {
     const iframe = document.getElementById(iframeId);
     const contentWindow = iframe.contentWindow;
 
+    // Reset the flag to false when reseting the scrolling operation
+    stopOperation = false; 
+
+    // Also reset the scrolling flag to false
+    scrolling = false; 
     pauseScrolling(backingTrackId);
     contentWindow.scrollTo(0, 0);
     currentScrollPosition = 0;
