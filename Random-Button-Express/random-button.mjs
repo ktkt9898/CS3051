f06c2a9ddee4616f08fa31e19ee26aa1744985a4
ktkt9898 <kyletruschel@gmail.com>
2025-03-30 18:09:39 +0200

    30-Mar-25 Random Button working with server images

--- Random-Button-Express/random-button.mjs
+++ Random-Button-Express/random-button.mjs
@@ -8,9 +8,27 @@ let server = app.listen(port, () => {
   console.log("To end the server, press 'CTRL+C'");
 });
 
-// Required to show pictures
+// Array of images
+const images = [
+  'random-images/grand-canyon.jpg',
+  'random-images/half-dome.jpg',
+  'random-images/mt-everest.jpg',
+  'random-images/niagara-falls.jpg',
+  'random-images/yellowstone.jpg',
+  'random-images/zion-national-park.jpg'
+];
+
+// Required to show images
 app.use(express.static('.'));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(process.cwd(), "random-button.html"));
+});
+
+// Endpoint to get a random image
+// Will display a random image from the array of images
+app.get('/random-image', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  const randomImage = images[randomIndex];
+  res.json({ imageUrl: randomImage });
 });
